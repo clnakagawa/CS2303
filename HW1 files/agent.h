8e19dd3b0ffa4688b5fe68b9fc0ab47f7a3291ab
clnakagawa <88415768+clnakagawa@users.noreply.github.com>
2021-08-03 20:31:42 -0700

    Add files via upload

---
+++ HW1 files/agent.h
@@ -0,0 +1,14 @@
+/*
+ * agent.h
+ *
+ *  Created on: Feb 5, 2021
+ *      Author: cln
+ */
+
+#ifndef SRC_AGENT_H_
+#define SRC_AGENT_H_
+
+void setTimeInterval(float timeInterval);
+void setEndLocation(float x, float y, float z);
+
+#endif /* SRC_AGENT_H_ */
---
+++ HW1 files/xMotor.c
@@ -0,0 +1,9 @@
+/*
+ * xMotor.c
+ *
+ *  Created on: Feb 5, 2021
+ *      Author: cln
+ */
+#include "xMotor.h"
+
+
